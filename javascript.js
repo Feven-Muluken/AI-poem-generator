72cf3e4acacd2da93ec4ed87f9d00ff14a86e921
Feven <feven@gmail.com>
2025-03-16 20:22:34 +0300

    commit css and js

--- javascript.js
+++ javascript.js
@@ -1,43 +1,18 @@
 function displayPoem(response) {
   console.log("poem generated")
   console.log(response.data.answer)
-  new Typewriter("#fool",{
+  new Typewriter("#writePoem",{
     strings: [response.data.answer],
     autoStart: true,
     delay: 1,
     cursor: "",
-    loop: false,
-    onComplete: function () {
-      // Ensure the text is preserved after the animation
-      document.querySelector("#fool").innerHTML = response.data.answer;
-    },
-
   });
-  // let poem = document.querySelector(".searching")
-  // poem.textContent(response.data.answer)
 };
-let EnterText = document.querySelector('.inputText');
-
-let searching = document.getElementById("poem");
-
 function generatePoem(event){
   event.preventDefault();
-  let Enter = `<p class="wait">⏳ Generating a poem about <span class="searchFor">${EnterText.value}</span></p>`;
-  searching.innerHTML = Enter;
-  let text = document.querySelector(".wait");
-  
-  function toggleText() {
-    if (text.style.display === 'none') {
-      text.style.display = 'block';
-      searching.style.height = "52px";
-    } else {
-      text.style.display = 'none';
-      searching.style.height = "52px";
-    }
-  }
-
-
-  setInterval(toggleText, 700);
+  let EnterText = document.querySelector('.inputText');
+  let searching = document.getElementById("poem");
+  let writePoem = document.getElementById("writePoem");
 
   let apiKey = "cb8540803900a4b63a58b24827o4tf1f";
   prompt = `Generate a poem about ${EnterText.value}`;
@@ -45,10 +20,14 @@ function generatePoem(event){
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
+
+  searching.innerHTML = `<p class="blink">⏳ Generating a poem about ${EnterText.value}</p>`;
+
+  let text = document.querySelector(".wait");
+  writePoem.classList.remove("hidden")
+  
   console.log(`prompt: ${prompt}`);
-  // console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
-  // console.log(displayPoem)
   
 };
 
